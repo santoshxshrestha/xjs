b8c2e94d988648865002af0db72e37eec3cc92cc
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-24 01:03:59 +0545

    update cnofig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,51 +3,32 @@ import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-    // JavaScript recommended rules
-    js.configs.recommended,
-
-    // React plugin recommended config
-    {
-        ...reactPlugin.configs.flat.recommended,
-        settings: {
-            react: {
-                version: "detect",
-            },
+  js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
-
-    // React JSX runtime config
-    {
-        ...reactPlugin.configs.flat["jsx-runtime"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
-
-    // Custom overrides for JS/JSX files
-    {
-        files: ["**/*.js", "**/*.jsx"],
-        languageOptions: {
-            parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module",
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-            },
-        },
-        rules: {
-            "react/no-unescaped-entities": "off",
-            "react/prop-types": "off",
-        },
-    },
-
-    // Prettier config (must be last to disable conflicting rules)
-    {
-        name: "prettier",
-        ...prettier,
-    },
+  },
+  prettier,
 ];
